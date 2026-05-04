32f3f8ea2e368edfdbabe9163b12dc72a30fa491
Sweetsan <coderintheworldhack100@gmail.com>
2026-05-04 15:53:02 +0530

    location and page tsx file created

---
+++ app/location/page.tsx
@@ -0,0 +1,13 @@
+import Address from "./address";
+
+function Page() {
+  let myName: string = "Sweetsan";
+  return (
+    <>
+      <h1> This is location Page</h1>
+      <Address />
+    </>
+  );
+}
+
+export default Page;
